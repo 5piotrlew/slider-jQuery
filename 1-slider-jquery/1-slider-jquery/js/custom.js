f74146b832e50c2fcbbc5d0c55172265a28cd7cc
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-24 17:03:00 +0200

    slide

--- 1-slider-jquery/1-slider-jquery/js/custom.js
+++ 1-slider-jquery/1-slider-jquery/js/custom.js
@@ -2,4 +2,13 @@
 
 $(function(){
     console.log('hej');
+    /*zmienne*/
+    var slideCount = $('.single-slide').length;
+    var slideShow = $('.slide-show');
+    /*Szerokosc kontenera*/
+    slideShow.css('width',slideCount * 100 + '%');
+    console.log(slideCount);
+    
+    
+    
 });
