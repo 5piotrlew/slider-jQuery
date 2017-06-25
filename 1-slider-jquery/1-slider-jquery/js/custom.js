678e5c6cd3fd6309abfe10892de78793078f6354
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-25 09:41:43 +0200

    %

--- 1-slider-jquery/1-slider-jquery/js/custom.js
+++ 1-slider-jquery/1-slider-jquery/js/custom.js
@@ -5,10 +5,22 @@ $(function(){
     /*zmienne*/
     var slideCount = $('.single-slide').length;
     var slideShow = $('.slide-show');
+   
+    var slideWidth = 100/slideCount;
     /*Szerokosc kontenera*/
     slideShow.css('width',slideCount * 100 + '%');
     console.log(slideCount);
     
+    //sto procent podziel na ilosc slajdow i foreach szerokosc taka do signleslide
     
+   // singleSlide.css('width', 100 / slideCount + '%');
     
+    //szerokosc obrazka iczem na kazdy element razy indeks
+    
+        $('.single-slide').each(function (index) {
+            $(this).css({'width': slideWidth + '%', 'margin-left': index * slideWidth + '%' });
+            
+        });
+    
+ 
 });
