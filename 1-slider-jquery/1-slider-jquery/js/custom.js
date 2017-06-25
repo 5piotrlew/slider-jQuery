7949ea886bc3e3a07a5567def6bba89a676e1c71
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-25 10:35:29 +0200

    works

--- 1-slider-jquery/1-slider-jquery/js/custom.js
+++ 1-slider-jquery/1-slider-jquery/js/custom.js
@@ -1,26 +1,50 @@
 'use strict';
 
-$(function(){
+$(function () {
     console.log('hej');
     /*zmienne*/
     var slideCount = $('.single-slide').length;
     var slideShow = $('.slide-show');
-   
-    var slideWidth = 100/slideCount;
+
+    var slideWidth = 100 / slideCount;
+    var slideIndex = 0;
     /*Szerokosc kontenera*/
-    slideShow.css('width',slideCount * 100 + '%');
+    slideShow.css('width', slideCount * 100 + '%');
     console.log(slideCount);
-    
+
     //sto procent podziel na ilosc slajdow i foreach szerokosc taka do signleslide
-    
-   // singleSlide.css('width', 100 / slideCount + '%');
-    
+
+    // singleSlide.css('width', 100 / slideCount + '%');
+
     //szerokosc obrazka iczem na kazdy element razy indeks
-    
-        $('.single-slide').each(function (index) {
-            $(this).css({'width': slideWidth + '%', 'margin-left': index * slideWidth + '%' });
-            
+
+    $('.single-slide').each(function (index) {
+        $(this).css({
+            'width': slideWidth + '%',
+            'margin-left': index * slideWidth + '%'
         });
-    
- 
+    });
+
+    $('.prev-slide').click(function () {
+
+
+    });
+
+    $('.next-slide').click(function () {
+        var newSlideIndex = slideIndex + 1;
+        if (newSlideIndex >= slideCount) {
+            return;
+        }
+
+        var marginLeft = (newSlideIndex * (-100)) + '%';
+        console.log(marginLeft);
+
+        slideShow.animate({
+            'margin-left': marginLeft
+        }, 800, function () {
+            slideIndex = newSlideIndex;
+
+        });
+
+    });
 });
